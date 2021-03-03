f1e9a7223b04490e9c1313a70fdd0e15424cbbe8
GPeBenito <g.pebenito821@gmail.com>
2021-03-03 12:21:06 +0100

    removed extraneous files and compiled form and JS into WeatherApp.js
    Deleted: CurrentWeather.js and .css
    Deleted: CurrentFeatures.js and .css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import"./styles.css";
 import WeatherApp from "./WeatherApp";
 import './App.css';
-import CurrentWeather from "./CurrentWeather";
-import CurrentFeatures from "./CurrentFeatures";
+
+
 
 function App() {
   return (
     <div className="App">
      <h1> React Weather App </h1>
      <WeatherApp city="New York"/>
-     <CurrentWeather/>
-     <CurrentFeatures/>
+    
+    
   
      <p> Open Source Code by <a href="https://github.com/GPeBenito/weather-react" target="_blank" rel="noreferrer"> G. Pe Benito </a> </p>
      </div>
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./WeatherApp.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function WeatherApp(props) {
   const [city, setCity] = useState(null);
@@ -58,17 +60,69 @@ export default function WeatherApp(props) {
           </button>
       </div>
       </div>
-
     </form>
     </div> 
   );
   if (loaded) {
     return (
       <div>
+        <div className="Weather">
         {form}
+              <h2> New York </h2>
+              <ul className="Header">
+                <li>Last updated: TUESDAY 10:00AM </li>
+                <li>Partly Cloudy</li>
+              </ul>
+              <div className="row">
+  
+              <div className="col-6">
+    
+              <img
+                src="https://i.pinimg.com/564x/77/0b/80/770b805d5c99c7931366c2e84e88f251.jpg"
+                alt="partly cloudy"
+                className="weather-icon float-left"
+              />
+  
+              <div className="float-left">
+                <span className="temperature">
+                  <p className="temp"> 19 </p>
+                </span>
+                <span className="units">
+                  <a
+                    href="https://www.google.com/search?q=what+is+celsius&rlz=1C5CHFA_enUS925US925&oq=what+is+celsius&aqs=chrome..69i57j0l5j0i395l4.2485j1j7&sourceid=chrome&ie=UTF-8"
+                    class="active"
+                  >
+                    ºC
+                  </a>
+                  |
+                  <a href="https://www.google.com/search?rlz=1C5CHFA_enUS925US925&sxsrf=ALeKk00kKnxxG69VpivFhAoCK25Zj3Sofw%3A1610790805093&ei=lbcCYLKeBczlkwXghq1Q&q=what+is+fahrenheit+based+on&oq=what+is+fa&gs_lcp=CgZwc3ktYWIQAxgAMgcIABDJAxBDMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAOgQIABBHOgQIIxAnOgQILhBDOgQIABBDUK6VAVj1pQFg0cMBaABwBHgAgAFWiAGpBZIBAjEwmAEAoAEBqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab">
+                    ºF
+                  </a>
+                </span>
+                </div>
+              </div>
+              <div className= "col-6">
+              <ul className="float-right">
+            <li>
+              {" "}
+               Humidity: 10 %
+            </li>
+            <li>
+              Wind: 5 km/h
+            </li>
+          </ul>
+          </div>
+          </div>
+           
+          </div>
+       
+      
+      
+     
+    
         <ul>
           <li>Temperature: {Math.round(weather.temperature)}ºC </li>
-          <li> Description: {weather.description}</li>
+          <li className= "text-capitalize"> Description: {weather.description}</li>
           <li>Humidity: {weather.humidity}%</li>
           <li> Wind: {weather.wind}km/h</li>
           <li>
