118dbb928c9842c36930ade1e91db7d94f6f72c7
GPeBenito <g.pebenito821@gmail.com>
2021-03-13 09:49:47 +0100

    Installed WeatherTemperature.js and celsius/fahrenheit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import "./WeatherInfo.css";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props){
     return (
         <div className="Info">
@@ -21,21 +22,9 @@ export default function WeatherInfo(props){
               
   </div>
               <div className="float-left">
-                <span className="temperature">
-                  <p className="temp"> {Math.round(props.data.temperature)}</p>
-                </span>
-                <span className="units">
-                  <a
-                    href="https://www.google.com/search?q=what+is+celsius&rlz=1C5CHFA_enUS925US925&oq=what+is+celsius&aqs=chrome..69i57j0l5j0i395l4.2485j1j7&sourceid=chrome&ie=UTF-8"
-                    class="active"
-                  >
-                    ºC
-                  </a>
-                  |
-                  <a href="https://www.google.com/search?rlz=1C5CHFA_enUS925US925&sxsrf=ALeKk00kKnxxG69VpivFhAoCK25Zj3Sofw%3A1610790805093&ei=lbcCYLKeBczlkwXghq1Q&q=what+is+fahrenheit+based+on&oq=what+is+fa&gs_lcp=CgZwc3ktYWIQAxgAMgcIABDJAxBDMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAOgQIABBHOgQIIxAnOgQILhBDOgQIABBDUK6VAVj1pQFg0cMBaABwBHgAgAFWiAGpBZIBAjEwmAEAoAEBqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab">
-                    ºF
-                  </a>
-                </span>
+              
+                <WeatherTemperature celsius={props.data.temperature}/>
+    
                 </div>
               </div>
               </div>
